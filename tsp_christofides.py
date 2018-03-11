423a383c5ceeeb0b2287aed3f91486451c730aa8
Jose <murrietj@oregonstate.edu>
2018-03-11 03:59:20 -0700

    implemented mst and odd degree vertices

---
+++ tsp_christofides.py
@@ -0,0 +1,94 @@
+# Authors: Elena Drake-Knight, Esther Fatehi, Jose Murrieta
+# Date: 3/10/2018
+# Description: This program reads a file specified in the command line argument.
+#   The file contains an instance of the travelling salesman problem. Each line
+#   defines a city and each line has three numbers sperated by white space. The
+#   first number is the city identifier, the second is the x-coordinate, and the
+#   third is the y-coordinate. This program then uses Christofides algorithm to
+#   solve the tsp and outputs the solution to a file with '.tour' appended to the
+#   input file name.
+
+import sys
+from math import sqrt
+
+class City(object):
+    def __init__(self, c_id=None, x=None, y=None, dist=None):
+        self.id = c_id
+        self.x = x
+        self.y = y
+
+def distance(city_a, city_b):
+    return round(sqrt((city_a.x - city_b.x)**2 + (city_a.y - city_b.y)**2))
+
+def mst_prim(adj_matrix):
+    Q = list(adj_matrix.keys())
+    keys = {c_id: float('inf') for c_id in Q}
+    keys[Q[0]] = 0
+    parents = {c_id: None for c_id in Q}
+    T = {}
+    while(keys):
+        u = min(keys, key = keys.get)
+        T[u] = {}
+        if parents[u]:
+            T[u][parents[u]] = T[parents[u]][u] = keys[u]
+        del keys[u]
+        for city, dist in adj_matrix[u].items():
+            if((city in keys) and adj_matrix[u][city] < keys[city]):
+                parents[city] = u
+                keys[city] = dist
+    return T
+
+def getOddDegVerts(adj_matrix):
+    O = set()
+    for city in adj_matrix.keys():
+        edges = len(adj_matrix[city])
+        if edges % 2 == 0:
+            O.add(city)
+    return O
+
+# open input file:
+fileName = sys.argv[1]
+inputFile = open(fileName, 'r')
+
+# create a complete graph G (adjacency matrix) from the file
+G = {}
+cities = [] 
+nextLine = inputFile.readline()
+while (nextLine):
+    c_id, c_x, c_y = nextLine.split()
+    newCity = City(c_id, int(c_x), int(c_y))
+    cities.append(newCity)
+    G[newCity.id] = {}
+
+    # add distances to new city to adjacency matrix
+    for city in cities:
+        G[city.id][newCity.id] = G[newCity.id][city.id] = distance(city, newCity)
+
+    nextLine = inputFile.readline()
+
+# close input file
+inputFile.close()
+
+# get minimum spanning tree T of G
+T = mst_prim(G)
+
+# Let O be the set of vertices with odd degree in T
+O = getOddDegVerts(T)
+
+# Find a minimum-weight perfect matching M in the induced subgraph given by the vertices from O
+# TODO
+
+# Combine the edges of M and T to form a connected multigraph H in which each vertex has even degree
+# TODO
+
+# Form an Eulerian circuit in H
+# TODO
+
+# Make the circuit found in the previous step into a Hamiltonian circuit by skipping repeated vertices
+# TODO
+
+# print results in output file
+#outputFile = open(fileName + '.tour', 'w')
+# TODO
+# close output file
+#outputFile.close()
