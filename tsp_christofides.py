6034205043d220d1cdf46fa3b09d6cf203669002
Jose <murrietj@oregonstate.edu>
2018-03-13 17:18:47 -0700

    fixed minWeightMatching()

--- tsp_christofides.py
+++ tsp_christofides.py
@@ -8,67 +8,67 @@
 #   solve the tsp and outputs the solution to a file with '.tour' appended to the
 #   input file name.
 
-import copy
-import sys
+from copy import deepcopy
+from sys import argv
 from math import sqrt
 
 class City(object):
-	def __init__(self, c_id=None, x=None, y=None, dist=None):
-		self.id = c_id
-		self.x = x
-		self.y = y
+    def __init__(self, c_id=None, x=None, y=None):
+        self.id = c_id
+        self.x = x
+        self.y = y
 
 def distance(city_a, city_b):
-	return round(sqrt((city_a.x - city_b.x)**2 + (city_a.y - city_b.y)**2))
+    return round(sqrt((city_a.x - city_b.x)**2 + (city_a.y - city_b.y)**2))
 
 def mst_prim(adj_matrix):
-	Q = list(adj_matrix.keys())
-	keys = {c_id: float('inf') for c_id in Q}
-	keys[Q[0]] = 0
-	parents = {c_id: None for c_id in Q}
-	T = {}
-	while(keys):
-		u = min(keys, key = keys.get)
-		T[u] = {}
-		if parents[u]:
-			T[u][parents[u]] = T[parents[u]][u] = keys[u]
-		del keys[u]
-		for city, dist in adj_matrix[u].items():
-			if((city in keys) and adj_matrix[u][city] < keys[city]):
-				parents[city] = u
-				keys[city] = dist
-	return T
+    Q = list(adj_matrix.keys())
+    keys = {c_id: float('inf') for c_id in Q}
+    keys[Q[0]] = 0
+    parents = {c_id: None for c_id in Q}
+    T = {}
+    while(keys):
+        u = min(keys, key = keys.get)
+        T[u] = {}
+        if parents[u]:
+            T[u][parents[u]] = T[parents[u]][u] = keys[u]
+        del keys[u]
+        for city, dist in adj_matrix[u].items():
+            if((city in keys) and adj_matrix[u][city] < keys[city]):
+                parents[city] = u
+                keys[city] = dist
+    return T
 
 def getOddDegVerts(adj_matrix):
-	O = set()
-	for city in adj_matrix.keys():
-		edges = len(adj_matrix[city])
-		if edges % 2 != 0:
-			O.add(city)
-	return O
+    O = set()
+    for city in adj_matrix.keys():
+        edges = len(adj_matrix[city])
+        if edges % 2 != 0:
+            O.add(city)
+    return O
 
-def minWeightMatching(G, O):
-	M = set()
-	while O:
-		u = O.pop()
-		length = float("inf")
-		closest = 0
-		for v in O:
-			if u != v and G[u][v] < length:
-				length = G[u][v]
-				closest = v
-		M.add((u, closest, length))
-		O.remove(closest)
-	return M
+def minWeightMatching(G, T, O):
+    M = set()
+    while O:
+        u = O.pop()
+        length = float("inf")
+        closest = 0
+        for v in O:
+            if u != v and G[u][v] < length and v not in T[u]:
+                length = G[u][v]
+                closest = v
+        M.add((u, closest, length))
+        O.remove(closest)
+    return M
 
 def combineGraphs(T, M):
-	H = T
-	while M:
-		u, v, dist = M.pop()
-		H[u][v] = H[v][u] = dist
-	return H
+    H = T
+    while M:
+        u, v, dist = M.pop()
+        H[u][v] = H[v][u] = dist
+    return H
 
-#helper function for eulerCircuit                
+# helper function for eulerCircuit                
 def dfs(H, u, visited):
     count = 1
     u = int(u)
@@ -80,9 +80,9 @@ def dfs(H, u, visited):
             count = count + dfs(H, i, visited)         
     return count
 
-#helper function for eulerCircuit 
+# helper function for eulerCircuit 
 def checkBridge(H, u, v):
-    R = copy.deepcopy(H) 
+    R = deepcopy(H) 
     if len(H[u]) == 1:
         return False 
     elif len(H[u]) == 0:
@@ -120,7 +120,7 @@ def getDistance(G, C):
     return total
             
 # open input file:
-fileName = sys.argv[1]
+fileName = argv[1]
 inputFile = open(fileName, 'r')
 
 # create a complete graph G (adjacency matrix) from the file
@@ -148,7 +148,7 @@ T = mst_prim(G)
 O = getOddDegVerts(T)
 
 # Find a minimum-weight perfect matching M in the induced subgraph given by the vertices from O
-M = minWeightMatching(G, O)
+M = minWeightMatching(G, T, O)
 
 # Combine the edges of M and T to form a connected multigraph H in which each vertex has even degree
 H = combineGraphs(T, M)
@@ -167,5 +167,6 @@ outputFile = open(fileName + '.tour', 'w')
 outputFile.write(str(total) + "\n")
 for i in C:
     outputFile.write(i + "\n")
+
 # close output file
 outputFile.close()
